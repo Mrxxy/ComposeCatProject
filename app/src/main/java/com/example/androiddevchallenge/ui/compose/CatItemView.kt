316c85c0dc1c074117da0759ca8368f9f0187b37
xiao <xych@gmail.com>
2021-03-02 14:31:07 +0800

    fix lint error

--- app/src/main/java/com/example/androiddevchallenge/ui/compose/CatItemView.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/compose/CatItemView.kt
@@ -3,7 +3,14 @@ package com.example.androiddevchallenge.compose
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
